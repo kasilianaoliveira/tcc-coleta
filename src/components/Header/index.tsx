98b73ad32383ea007e6d06bda164ec0076ba5b7f
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-11-16 15:26:49 -0300

    Update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from 'react';
+import { ReactNode, useContext } from 'react';
 import { Link } from "react-router-dom";
 
 import Logo from '../../assets/logo.svg'
 import './style.css'
+import { AuthContext } from '../../contexts/AuthContext';
 
 interface HeaderProps {
   children?: ReactNode;
@@ -11,28 +12,45 @@ interface HeaderProps {
 
 
 export const Header = ({ children, isHome = false }: HeaderProps) => {
+
+  const { user } = useContext(AuthContext);
+
   return (
     <header className='header'>
       <Link to='/'>
-      <img src={Logo} alt="" />
+        <img src={Logo} alt="" />
       </Link>
 
       <nav className='menu'>
-        {isHome && <ul>
-          <li>
-            <Link to='/cadastro'>
-              Cadastro
-            </Link>
-          </li>
-          <li>
-            <Link to='/login'>
-              Login
-            </Link>
-          </li>
-          <li>
-            Meu painel
-          </li>
-        </ul>}
+        {isHome &&
+          <ul>
+            <li>
+              <Link to='/cadastro'>
+                Cadastro
+              </Link>
+            </li>
+            {
+              !user && (
+                <>
+
+                  <li>
+                    <Link to='/login'>
+                      Login
+                    </Link>
+                  </li>
+                </>
+              )
+            }
+            {
+              user && (
+                <li>
+                  <Link to='/perfil'>
+                    Acessar meu painel
+                  </Link>
+                </li>
+              )
+            }
+          </ul>}
 
         {children}
       </nav>
