f0ac5d0278daac7d08d2ff21ff4bd297c49176ee
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-11-22 07:24:21 -0300

    Fix reloaded da pagina

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,9 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import avatarImg from '../../assets/avatar.png'
 import { Link, useNavigate } from 'react-router-dom'
 
-import { FiHome,  FiSettings } from 'react-icons/fi'
+import { FiHome, FiSettings } from 'react-icons/fi';
+import { TbHomeEdit } from "react-icons/tb";
 import './style.css';
 import { AuthContext } from '../../contexts/AuthContext';
 import { CustomButton } from '../CustomButton';
@@ -12,9 +13,10 @@ import Cookies from 'js-cookie';
 
 export default function Sidebar() {
   const { logout } = useContext(AuthContext);
-  const { pointList,getPoint } = useContext(PointContext);
+  const { pointList, getPoint, reLoaded, pointNotFound } = useContext(PointContext);
   const navigate = useNavigate();
 
+  console.log(pointNotFound)
   const handleLogout = () => {
     logout();
     navigate('/')
@@ -24,17 +26,19 @@ export default function Sidebar() {
     const token = Cookies.get('auth.token');
     const { sub } = jwtDecode(token);
     if (token) {
-      getPoint(sub)
+      getPoint(sub);
     }
-  }, []);
+    if (pointList === undefined) {
+      navigate('/perfil')
+    }
+  }, [reLoaded]);
 
-  console.log(pointList)
   const notImage = 'http://localhost:3333/uploads/null';
 
   return (
     <div className="sidebar">
       <div>
-        {pointList && pointList.image !== notImage ? (
+        {!pointNotFound && pointList && pointList.image !== notImage && pointList.image !== undefined ? (
           <img src={pointList.image} alt="Foto de perfil" />
         ) : (
           <img src={avatarImg} alt="Foto de perfil" />
@@ -43,15 +47,26 @@ export default function Sidebar() {
 
       <ul className='sidebar-list'>
         {
-          pointList ? (
-            <li>
-              <Link to="/editar-ponto">
+          !pointNotFound && pointList ? (
+            <>
+              <li>
+                <Link to="/editar-ponto">
 
-                <FiHome size={24} className="icon"
-                />
-                Ponto de coleta
-              </Link>
-            </li>
+                  <FiHome size={24} className="icon"
+                  />
+                  Ponto de coleta
+                </Link>
+              </li>
+              <li>
+                <Link to="/editar-bairro">
+
+                  <TbHomeEdit size={24} className="icon"
+                  />
+                  Meu(s) bairro(s)
+                </Link>
+              </li>
+            </>
+
 
           ) : (
             <li>
